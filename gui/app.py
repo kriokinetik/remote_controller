0107fe6d6de7cc9e6574ac2ab101b2d7f5f114a4
kriokinetik <sadiev2005@mail.ru>
2025-04-05 00:46:29 +0300

    Change logger file mode from write to append

--- gui/app.py
+++ gui/app.py
@@ -9,7 +9,7 @@ def start_gui():
     app = QApplication(sys.argv)
     window = RemoteControllerWindow()
 
-    file_handler = logging.FileHandler(filename=LOG_FILE, mode="w", encoding="utf-8")
+    file_handler = logging.FileHandler(filename=LOG_FILE, mode="a", encoding="utf-8")
     app_handler = TextHandler(window.log_output)
     logging.basicConfig(format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
                         datefmt="%d-%m-%Y %H:%M:%S",
