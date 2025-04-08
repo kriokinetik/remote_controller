8dd7998f0e76f271ce1e599a61334936fc790e3a
kriokinetik <sadiev2005@mail.ru>
2025-04-08 03:21:12 +0300

    Fix crash on startup when config.json is missing or empty

--- config.py
+++ config.py
@@ -2,18 +2,23 @@ import json
 
 
 def get_config() -> {}:
-    with open("config.json") as f:
-        data = json.load(f)
+    try:
+        with open(CONFIG_FILE) as f:
+            data = json.load(f)
 
-    config = {
-        "token": data["TOKEN"],
-        "admin": data["ADMIN"],
-        "yandex": {
-            "token": data["YANDEX"]["TOKEN"],
-            "id": data["YANDEX"]["ID"],
-            "secret": data["YANDEX"]["SECRET"]
+        config = {
+            "token": data.get("TOKEN", ""),
+            "admin": data.get("ADMIN", []),
+            "yandex": {
+                "token": data.get("YANDEX", {}).get("TOKEN", ""),
+                "id": data.get("YANDEX", {}).get("ID", ""),
+                "secret": data.get("YANDEX", {}).get("SECRET", "")
+            }
         }
-    }
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+            f.write(json.dumps({}, indent=4, ensure_ascii=False))
+        return {}
 
     return config
 
@@ -22,6 +27,7 @@ SCREENSHOT_NAME = "screenshot.png"
 YANDEX_FOLDER = "remote_controller"
 LOG_FILE = "remote_controller.log"
 MISC_FOLDER = "./misc"
+CONFIG_FILE = "config.json"
 MAX_SIZE = 52428800
 MAX_MESSAGE_LENGTH = 4096
 PAGE_SIZE = 10
